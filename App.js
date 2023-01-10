dcbd62f91f618a29b37e1c22caec4b2b72a74d6f
Darwin Cacuango <dcacuango@cobiscorp.com>
2023-01-10 16:31:24 -0500

    preubas

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("process.env.",process.env)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{process.env.REACT_APP_FIREBASE_MEASUREMENT_ID+""}</Text>
       <StatusBar style="auto" />
     </View>
   );
